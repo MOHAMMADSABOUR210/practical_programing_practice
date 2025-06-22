e8ba23881119feb28ecd7c0ce808388c3d7e2ad6
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-06-22 22:36:54 +0330

    digit_3.py Problem and solution

---
+++ digit_3.py
@@ -0,0 +1,10 @@
+Three_digit=[]
+for i in range(100,1000):
+    if i % 3 == 0:
+            string_i = str(i)
+            if (string_i[0] == string_i[1] ) or (string_i[0] == string_i[2]) or (string_i[1] == string_i[2]):
+                if  not (string_i[0] == string_i[1] == string_i[2] ):
+                    Three_digit.append(i)
+print(Three_digit)
+
+# Finding three-digit numbers divisible by 3 that are made with only two different numbers
